576497dd2f6379de0e8713d1945694e4237a201a
thdtjdals__ <144472331+tjdals4716@users.noreply.github.com>
2024-05-24 02:20:41 +0900

    :white_check_mark: Test 클래스 작성 완료

---
+++ src/Algorithm/Test.java
@@ -0,0 +1,13 @@
+package Algorithm;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] arg){
+        System.out.printf("souf : 서식이 지정된 문자열을 System.out으로 출력");
+        System.out.println("sout : 문자열을 System.out으로 출력");
+        System.out.println("soutm : 현재 클래스 및 메서드 이름을 System.out으로 출력");
+        System.out.println("soutp : 메서드 매개변수 이름 및 값을 System.out으로 출력");
+        System.out.println("soutv : 값을 System.out으로 출력");
+    }
+}
