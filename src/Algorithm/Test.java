802cedaedcbeb1bfd00191a499e15bee0353a7e0
thdtjdals__ <144472331+tjdals4716@users.noreply.github.com>
2024-05-26 01:27:08 +0900

    :white_check_mark: 2일차 스터디

--- src/Algorithm/Test.java
+++ src/Algorithm/Test.java
@@ -5,6 +5,13 @@ public class Test {
         int a = 1;
         int b = 3;
         int sum = a + b;
-        System.out.printf("a + b = " + sum);
+        int diff = a - b;
+        int muti = a * b;
+        int devi = a / b;
+
+        System.out.println("a + b = " + sum);
+        System.out.println("a - b = " + diff);
+        System.out.println("a * b = " + muti);
+        System.out.println("a / b = " + devi);
     }
 }
