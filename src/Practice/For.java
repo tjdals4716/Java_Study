4e94ceefaccaa1a8f1f5187369d1c365f95fe75e
thdtjdals__ <144472331+tjdals4716@users.noreply.github.com>
2024-05-27 14:48:48 +0900

    :white_check_mark: 3일차 스터디

--- src/Practice/For.java
+++ src/Practice/For.java
@@ -3,9 +3,11 @@ package Practice;
 //1부터 10까지의 합을 출력하는 프로그램
 public class For {
     public static void main(String[] args) {
-        int sum = 0; //합계를 저장할 변수를 초기화, 처음에는 0으로 설정
+        //합계를 저장할 변수를 초기화, 처음에는 0으로 설정
+        int sum = 0;
         for(int i = 1;i < 11;i++){
-            sum += i; //반복문이 실행될 때마다 sum에 현재 i 값을 더하고 그 결과를 sum에 저장
+            //반복문이 실행될 때마다 sum에 현재 i 값을 더하고 그 결과를 sum에 저장
+            sum += i;
         }
         System.out.printf("1부터 10까지의 합은 " + sum + "입니다.");
     }
--- src/Practice/Scan_String.java
+++ src/Practice/Scan_String.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 //사용자로부터 값을 입력받아 이름과 나이를 출력하는 프로그램
 public class Scan_String {
     public static void main(String[] arg){
-        Scanner scanner = new Scanner(System.in); //표준 입력 스트림을 사용하여 Scanner 객체를 생성
+        //표준 입력 스트림을 사용하여 Scanner 객체를 생성
+        Scanner scanner = new Scanner(System.in);
 
         System.out.printf("이름을 입력하세요 : ");
         String name = scanner.nextLine();
