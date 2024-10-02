b1f4efb2650e73a88d7cf669072fe64409838061
tjdals4716 <song47166@naver.com>
2024-10-02 12:45:52 +0900

    :white_check_mark: 5주차 2차시

---
+++ src/Week5_Session2/CircleArray.java
@@ -0,0 +1,36 @@
+package Week5_Session2;
+
+class Circle {
+    int radius;
+
+    public Circle(int radius){
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        return 3.14 * radius * radius;
+    }
+
+    public int getSum(int i, int j){
+        return i + j;
+    }
+
+    public double getSum(double i, double j){
+        return(double)(i + j);
+    }
+}
+
+public class CircleArray {
+    public static void main(String[] args) {
+        Circle[] c;
+        c = new Circle[5];
+
+        for(int i = 0;i < c.length;i++){
+            c[i] = new Circle(i);
+        }
+        for(int i = 0;i < c.length;i++){
+            System.out.print((int)(c[i].getArea()) + " ");
+        }
+    }
+}
+
